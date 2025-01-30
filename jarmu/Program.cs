aa3c83481885481ff44d317f8b444a022a0eceaf
blekhol <peiszr@gmail.com>
2025-01-30 17:05:51 +0100

    kész

--- jarmu/Program.cs
+++ jarmu/Program.cs
@@ -10,7 +10,13 @@ namespace jarmu
     {
         static void Main(string[] args)
         {
-            
+            Sportauto a1 = new Sportauto("Ferrari", "488 GTB", 2020, 330);
+            Console.WriteLine(a1);
+
+            a1.Gyorsit(50);
+            a1.Fekez(20);
+
+            Console.WriteLine(1);
         }
     }
 }
